195c9052a4f450e86faf4604c9b22b37909bcbf0
Aaron Bronow <aaron@aaronbronow.com>
2013-11-11 11:17:49 -0800

    adding goodreads

--- Gemfile
+++ Gemfile
@@ -43,3 +43,5 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+gem "goodreads", "~> 0.3.0"
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    # testing good reads connection
+    client = Goodreads::Client.new(:api_key => 'QTFfEaugOxPvTIrzGy0TQ', :api_secret => 'doVGGGX7lzIv0XDXZKfGGw1bYzDIkPoefQptZsfJqQ')
+    
+    # testing good reads query
+    author = client.author_by_name('Neal Stephenson')
+    
+    @message = "Hello, "
+    @author_link = author.link
   end
 end
