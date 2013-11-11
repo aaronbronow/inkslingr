7c43937330efa3d09efc30e57d811301cb944a7a
Aaron Bronow <aaron@aaronbronow.com>
2013-11-11 11:07:18 -0800

    adding empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inkslingr::Application.config.secret_key_base = '4b97ee185cad751cb480a21ca5f418e5f7bbbe595735572fed40f8e8d127eea10816af8f9d1a7b9224dba09f6a4c295da4712e8205285071741632d79f782e04'
