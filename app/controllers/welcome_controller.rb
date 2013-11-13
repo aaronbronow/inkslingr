453b53f97293b4b03ed40684c9c265f6b3b4e927
Aaron Bronow <aaron@aaronbronow.com>
2013-11-13 14:22:15 -0800

    adding sign in with twitter link

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,11 +9,11 @@ class WelcomeController < ApplicationController
     @message = "Hello, "
     @author_link = author.link
     
-    twitter = Twitter::Client.new(
-      :oauth_token => '117190492-uyi7pm5Gfq24pOwREFTYqgqE0onE9Zesmi7lpqIM',
-      :oauth_token_secret => 'FnxsYmz6kI5e8LoCnr1Go9TYChSmVgPnHQNCtT4Psepy8'
-    )
+    #twitter = Twitter::Client.new(
+    #  :oauth_token => '117190492-uyi7pm5Gfq24pOwREFTYqgqE0onE9Zesmi7lpqIM',
+    #  :oauth_token_secret => 'FnxsYmz6kI5e8LoCnr1Go9TYChSmVgPnHQNCtT4Psepy8'
+    #)
     
-    @results = twitter.user_search('Neal Stephenson', :count => 1, :result_type => 'popular')
+    #@results = twitter.user_search('Neal Stephenson', :count => 1, :result_type => 'popular')
   end
 end
